c9f92c8f2b5625f3066619eab3d7f2b22ce1abd5
kokok <sunupf@gmail.com>
2016-11-30 05:56:24 +0700

    add negation rule again

--- generate.js
+++ generate.js
@@ -38,7 +38,23 @@ var data  = function(){
           case 'min':
             var min = parseInt(size[1])
             if(min >= 0){
-              specialObject.min= min
+              if(!specialObject.notMax){
+                specialObject.min= min
+              }
+            }else if(typeof size[1] === "undefined" || size[1] === "" ){
+              throw new Error("Minimal size is undefined, please check your syntax at '"+n+"'")
+            }else if(min<0){
+              throw new Error("Minimal size is less than 0")
+            }
+            break;
+          case '^min':
+            var min = parseInt(size[1])
+            if(min >= 1){
+              specialObject.max= min-1
+              specialObject.notMin = true;
+            }else if(min === 0){
+              specialObject.pattern = null
+              specialObject.notMin = true;
             }else if(typeof size[1] === "undefined" || size[1] === "" ){
               throw new Error("Minimal size is undefined, please check your syntax at '"+n+"'")
             }else if(min<0){
@@ -48,7 +64,20 @@ var data  = function(){
           case 'max':
             var max = parseInt(size[1])
             if(max >= 0){
-              specialObject.max= max
+              if(!specialObject.notMin){
+                specialObject.max= max
+              }
+            }else if(typeof size[1] === "undefined" || size[1] === "" ){
+              throw new Error("Maximal size is undefined, please check your syntax at '"+n+"'")
+            }else if(min<0){
+              throw new Error("Maximal size is less than 0")
+            }
+            break;
+          case '^max':
+            var max = parseInt(size[1])
+            if(max >= 1){
+              specialObject.min= max+1
+              specialObject.notMax = true
             }else if(typeof size[1] === "undefined" || size[1] === "" ){
               throw new Error("Maximal size is undefined, please check your syntax at '"+n+"'")
             }else if(min<0){
@@ -60,6 +89,21 @@ var data  = function(){
             if(exactSize>=0){
               specialObject.min = exactSize
               specialObject.max = exactSize
+            }else if(typeof size[1] === "undefined" || size[1] === ""){
+              throw new Error("Exact size is undefined")
+            }else if(exactSize<0){
+              throw new Error("Exact size is less than 0")
+            }else{
+              throw new Error("Unknown Errors")
+            }
+            break;
+          case '^exact':
+            var exactSize = parseInt(size[1])
+            if(exactSize>=0){
+              specialObject.min = exactSize+1
+              specialObject.max = exactSize-1
+              specialObject.notMin = true
+              specialObject.notMax = true
             }else if(typeof size[1] === "undefined" || size[1] === ""){
               throw new Error("Exact size is undefined")
             }else if(exactSize<0){
@@ -104,117 +148,48 @@ var data  = function(){
               throw new Error("Minimal and Maximal Size are undefined, please check yout syntax at '"+n+"'")
             }
             break;
+          case '^between':
+            if(typeof size[1] != "undefined" && size[1] != ""){
+              // throw new Error(size)
+              var betweenSize = size[1].split(",")
+
+              if(
+                typeof betweenSize[0] != "undefined" &&
+                typeof betweenSize[1] != "undefined" &&
+                parseInt(betweenSize[0]) <= parseInt(betweenSize[1])
+              ){
+                specialObject.min = parseInt(betweenSize[0])+1
+                specialObject.max = parseInt(betweenSize[1])-1
+                specialObject.notMin = true
+                specialObject.notMax = true
+
+                if(specialObject.min<0){
+                  throw new Error("Minimal size is less than 0")
+                }
+                if(specialObject.max<0){
+                  throw new Error("Minimal size is less than 0")
+                }
+              }else if(typeof betweenSize[0] === "undefined" || betweenSize[0] === ""){
+                // Throw Error
+                throw new Error("Minimal size is undefined, please check your syntax at '"+n+"'")
+              }else if(typeof betweenSize[1] === "undefined" || betweenSize[1] === ""){
+                // Throw Error
+                throw new Error("Maximal size is undefined, please check your syntax at '"+n+"'")
+              }else if(parseInt(betweenSize[0])>parseInt(betweenSize[1])){
+                // Throw Error
+                throw new Error("Minimal Size has bigger value than Maximal Size")
+              }else{
+                throw new Error("Unknown Error")
+              }
+            }else{
+              // Throw Error
+              throw new Error("Minimal and Maximal Size are undefined, please check yout syntax at '"+n+"'")
+            }
+            break;
         }
       }
     })
 
-    /* // try{
-    //   _.forEach(arrayOfValidation,function(n,key){
-    //
-    //     var size = n.split(':')
-    //     if(size.length > 0 && size.length<=2){
-    //       switch(size[0]){
-    //         case 'min':
-    //           var min = parseInt(size[1])
-    //           if(min >= 0){
-    //             specialObject.sizeMin = min
-    //           }else if(typeof size[1] === "undefined" || size[1] === "" ){
-    //             throw new Error("Minimal size is undefined, please check your syntax at '"+n+"'")
-    //           }else if(min<0){
-    //             throw new Error("Minimal size is less than 0")
-    //           }
-    //           break;
-    //         case 'max':
-    //           var min = parseInt(size[1])
-    //           if(min >= 0){
-    //             specialObject.sizeMax = parseInt(size[1])
-    //           }else if(typeof size[1] === "undefined" || size[1] === "" ){
-    //             throw new Error("Maximal size is undefined, please check your syntax at '"+n+"'")
-    //           }else if(min<0){
-    //             throw new Error("Maximal size is less than 0")
-    //           }
-    //           break;
-    //         case 'exact':
-    //           var exactSize = parseInt(size[1])
-    //           if(exactSize>=0){
-    //             specialObject.sizeMin = exactSize
-    //             specialObject.sizeMax = exactSize
-    //           }else if(typeof size[1] === "undefined" || size[1] === ""){
-    //             throw new Error("Exact size is undefined")
-    //           }else if(exactSize<0){
-    //             throw new Error("Exact size is less than 0")
-    //           }else{
-    //             throw new Error("Unknown Errors")
-    //           }
-    //           break;
-    //         case 'between':
-    //           if(typeof size[1] != "undefined" && size[1] != ""){
-    //             // throw new Error(size)
-    //             var betweenSize = size[1].split(",")
-    //
-    //             if(
-    //               typeof betweenSize[0] != "undefined" &&
-    //               typeof betweenSize[1] != "undefined" &&
-    //               parseInt(betweenSize[0]) <= parseInt(betweenSize[1])
-    //             ){
-    //               specialObject.sizeMin = parseInt(betweenSize[0])
-    //               specialObject.sizeMax = parseInt(betweenSize[1])
-    //               if(specialObject.sizeMin<0){
-    //                 throw new Error("Minimal size is less than 0")
-    //               }
-    //               if(specialObject.sizeMax<0){
-    //                 throw new Error("Minimal size is less than 0")
-    //               }
-    //             }else if(typeof betweenSize[0] === "undefined" || betweenSize[0] === ""){
-    //               // Throw Error
-    //               throw new Error("Minimal size is undefined, please check your syntax at '"+n+"'")
-    //             }else if(typeof betweenSize[1] === "undefined" || betweenSize[1] === ""){
-    //               // Throw Error
-    //               throw new Error("Maximal size is undefined, please check your syntax at '"+n+"'")
-    //             }else if(parseInt(betweenSize[0])>parseInt(betweenSize[1])){
-    //               // Throw Error
-    //               throw new Error("Minimal Size has bigger value than Maximal Size")
-    //             }else{
-    //               throw new Error("Unknown Error")
-    //             }
-    //           }else{
-    //             // Throw Error
-    //             throw new Error("Minimal and Maximal Size are undefined, please check yout syntax at '"+n+"'")
-    //           }
-    //           break;
-    //         case 'required':
-    //           if(!notMatch){
-    //             specialObject.sizeMin = 1
-    //           }else if(specialObject.pattern === "." && notMatch){
-    //             specialObject.pattern = "[ ]"
-    //           }else{
-    //
-    //           }
-    //           break;
-    //         case 'alpha' :
-    //         case 'alpha_dash' :
-    //         case 'alpha_num' :
-    //         case 'email' :
-    //           if(notMatch){
-    //             specialObject.pattern = availablePattern[size[0]].notMatch
-    //           }else{
-    //             specialObject.pattern = availablePattern[size[0]].pattern
-    //           }
-    //           if(size[0] === 'email'){
-    //             delete specialObject.sizeMin;
-    //           }
-    //           break;
-    //         default:
-    //           throw new Error("Unsupported validation")
-    //           break;
-    //       }
-    //     }else{
-    //       throw new Error("Unsupported validation")
-    //     }
-    //   })
-    // // }catch(e){
-    //   // return e
-    // // } */
     return specialObject;
   }
   /**
@@ -223,11 +198,8 @@ var data  = function(){
    * @param {String} mode mode for RegExp {i,g,...}
    * @return {Object} Regular Expression Object
    */
-  function buildRegex(specialObject, mode){
+  function buildRegex(specialObject){
     if(specialObject.pattern){
-      if(typeof mode === "undefined"){
-        mode = ""
-      }
       var size = getRegexSize(specialObject);
       var regexSize;
 
@@ -245,58 +217,35 @@ var data  = function(){
       throw new Error("Couldn't generate regex because of undefined pattern")
     }
   }
-  /**
-   * Build regex from special object for not match option
-   * @param {Object} specialObject Special Object
-   * @param {String} mode mode for RegExp {i,g,...}
-   * @return {Object} Regular Expression Object
-   */
-  function buildNotMatchRegex(specialObject, mode){
-    if(specialObject.pattern){
-      if(typeof mode === "undefined"){
-        mode = ""
-      }
-      var size = getRegexSize(specialObject);
-      var regexSize;
 
-      regexSize = size.join();
-      if(regexSize){
-        if(size[0]>0 && size[1]>0){
-          return new RegExp("^("+specialObject.pattern+"{0,"+(size[0]-1)+"}|"+specialObject.pattern+"{"+(size[1]+1)+",})$",mode);
-        }
-        if(size[0] === 0){
-          return new RegExp("^"+specialObject.pattern+"{"+(size[1]+1)+",}$",mode);
-        }
-        if(size[1] === ""){
-          return new RegExp("^"+specialObject.pattern+"{0,"+(size[0]-1)+"}$",mode);
-        }
-      }else{
-        if(specialObject.pattern[(specialObject.pattern.length-1)] != "}"){
-          specialObject.pattern += "*"
-        }
-        return new RegExp("^"+specialObject.pattern+"$",mode);
-      }
-    }else{
-      throw new Error("Couldn't generate regex because of undefined pattern")
-    }
-  }
   /**
   * Get regex rule Size
   * @param {Object} specialObject Special Object
   * @return {Mixed} String when success {x,y} x is min, and y is max, and FALSE when fail
   */
   function getRegexSize(specialObject){
-    if(specialObject.sizeMin && specialObject.sizeMax){
-      if(specialObject.sizeMin < 0 && specialObject.sizeMax < 0){
+    if(specialObject.min && specialObject.max){
+      if(specialObject.min < 0 && specialObject.max < 0){
         // return new Error("Minimal and Maximal size is less than zero")
         return []
       }else{
-        if(!isNaN(specialObject.sizeMin) && !isNaN(specialObject.sizeMax)){
-          var size = [specialObject.sizeMin,specialObject.sizeMax]
-        }else if(isNaN(specialObject.sizeMin) && !isNaN(specialObject.sizeMax)){
+        if(!isNaN(specialObject.min) && !isNaN(specialObject.max)){
+          if(specialObject.notMax && specialObject.notMin){
+            var size = [
+              [0,specialObject.max],
+              [specialObject.min,""]
+            ]
+          }else if (specialObject.notMin){
+            var size = [0,specialObject.max]
+          }else if (specialObject.notMax){
+            var size = [specialObject.min,""]
+          }else{
+            var size = [specialObject.min,specialObject.max]
+          }
+        }else if(isNaN(specialObject.min) && !isNaN(specialObject.max)){
           // return new Error("Minimal size is not a number")
           return []
-        }else if(!isNaN(specialObject.sizeMin) && isNaN(specialObject.sizeMax)){
+        }else if(!isNaN(specialObject.min) && isNaN(specialObject.max)){
           // return new Error("Maximal size is not a number")
           return []
         }else{
@@ -304,25 +253,25 @@ var data  = function(){
           return []
         }
       }
-    }else if(specialObject.sizeMin){
-      if(specialObject.sizeMin < 0){
+    }else if(specialObject.min){
+      if(specialObject.min < 0){
         // return new Error("Minimal size is less than zero")
         return []
       }else{
-        if(!isNaN(specialObject.sizeMin)){
-          var size = [specialObject.sizeMin,""]
+        if(!isNaN(specialObject.min)){
+          var size = [specialObject.min,""]
         }else{
           // return new Error("Minimal size is not a number")
           return []
         }
       }
-    }else if(specialObject.sizeMax){
-      if(specialObject.sizeMax < 0){
+    }else if(specialObject.max){
+      if(specialObject.max < 0){
         // return new Error("Maximal size is less than zero")
         return []
       }else{
-        if(!isNaN(specialObject.sizeMax)){
-          var size = [0,specialObject.sizeMax];
+        if(!isNaN(specialObject.max)){
+          var size = [0,specialObject.max];
         }else{
           // return new Error("Maximal size is not a number")
           return []
@@ -340,7 +289,6 @@ var data  = function(){
     'stringToArray' : stringToArray,
     'arrayToSpecialObj' : arrayToSpecialObj,
     'buildRegex' : buildRegex,
-    'buildNotMatchRegex' : buildNotMatchRegex,
     'getRegexSize' : getRegexSize,
     'generate' : generate,
   }
